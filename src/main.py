6efdca0426103d7dc496743e7fe86f3d492b106e
brettlazarine <jblazarine@gmail.com>
2024-10-04 13:43:00 -0500

    refactor, text_node_to_html

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
-from textnode import *
-from htmlnode import *
+from textnode import TextNode
+
 
 def main():
-    text_node = TextNode("This is a test TextNode", "bold", "https://test")
-    print(text_node)
-    html_node = HTMLNode("div", "Hello World!", props={"class": "container"})
-    print(html_node)
+    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(node)
 
 
 main()
