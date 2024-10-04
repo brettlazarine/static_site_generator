24a3112a5e97383ec0147b7c54a7417934ede6c1
brettlazarine <jblazarine@gmail.com>
2024-10-04 15:34:17 -0500

    add inline delimiter

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
-from textnode import TextNode
+from textnode import *
 
 
 def main():
-    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(node)
+    code_node = TextNode("This is text with a `code block` word", text_type_text)
+    new_code_nodes = split_nodes_delimiter([code_node], "`", text_type_code)
+    print(new_code_nodes)
+    bold_node = TextNode("This is text with a **bold word**", text_type_text)
+    new_bold_nodes = split_nodes_delimiter([bold_node], "**", text_type_bold)
+    print(new_bold_nodes)
+    italic_node = TextNode("This is text with an *italic word*", text_type_text)
+    new_italic_nodes = split_nodes_delimiter([italic_node], "*", text_type_italic)
+    print(new_italic_nodes)
 
 
 main()
